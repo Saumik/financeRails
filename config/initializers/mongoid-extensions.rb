ac4e0c5060f19566658863836c7cd8b7732a840c
Moshe Bergman <bergmanm@gmail.com>
2013-04-07 19:20:40 -0700

    support for regular date format too

--- config/initializers/mongoid-extensions.rb
+++ config/initializers/mongoid-extensions.rb
@@ -2,7 +2,11 @@ module Mongoid
   module Extensions
     module String
       def __convert_to_date__(str, format=:default)
-        ::Date.strptime(str, I18n.translate("date.formats.#{format}")).strftime('%Y-%m-%d %H:%M:%S')
+        if str.include? '/'
+          ::Date.strptime(str, I18n.translate("date.formats.#{format}")).strftime('%Y-%m-%d %H:%M:%S')
+        else
+          str
+        end
       end
 
       def __mongoize_time__

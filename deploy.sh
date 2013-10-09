252af73c803590bcc781799785718c7096b76094
Moshe Bergman <bergmanm@gmail.com>
2013-10-09 11:32:26 -0700

    Updated deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-cd /home/mb/www/financeRails/public/
+cd /srv/www/finance.moshebergman.com/public_html/
 git pull
-bundle install --path ../shared/bundle/
+bundle install --path ../bundle/
 RAILS_ENV=production bundle exec rake assets:precompile
 touch tmp/restart.txt

70d13c31d81d3fe942b26fb10b968991c5ea670e
Moshe Bergman <bergmanm@gmail.com>
2013-10-09 22:14:51 -0700

    added origin master to deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 cd /srv/www/finance.moshebergman.com/public_html/
-git pull
+git pull origin master
 bundle install --path ../bundle/
 RAILS_ENV=production bundle exec rake assets:precompile
 touch tmp/restart.txt

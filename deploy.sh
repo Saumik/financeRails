2aa4bd539f5e83e63c8175291c9b2c0f5d4ae17b
Moshe Bergman <bergmanm@gmail.com>
2013-10-09 22:33:24 -0700

    bundle without deployment and test on production

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 cd /srv/www/finance.moshebergman.com/public_html/
 git pull origin master
-bundle install --path ../bundle/
+bundle install --path ../bundle/ --without development test
 RAILS_ENV=production bundle exec rake assets:precompile
 touch tmp/restart.txt

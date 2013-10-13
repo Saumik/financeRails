a134640784deab23284d1a35b18b2b94c763a2e7
Moshe Bergman <bergmanm@gmail.com>
2013-10-13 08:14:50 -0700

    add ruby version

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 cd /srv/www/finance.moshebergman.com/public_html/
+rvm use 1.9.3-p448@finance
 git pull origin master
 bundle install --path ../bundle/ --without development test
 RAILS_ENV=production bundle exec rake assets:precompile
